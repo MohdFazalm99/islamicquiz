eed46e93ba39266a5bab63ee5b5924e972726338
Mohdfazalm99 <mohammadfazalm@gmail.com>
2020-05-30 04:05:12 +0530

    New working files

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import ReactDOM from "react-dom"
+import "/home/fazal/Documents/islamicquiz/src/assets/style.css";
+import quizService from './quizService';
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result"
+
+class Isalamicquiz extends Component {
+    state = {
+        questionBank:[],
+        score:0,
+        responses:0
+
+    };
+    getQuestions =() => {
+        quizService().then(question => {
+            this.setState({
+                questionBank:question
+            })
+        })
+    };
+    computeAnswer = (answers,correctAnswer) => {
+        if(answers === correctAnswer){
+            this.setState({
+                score:this.state.score + 1
+            })
+
+        }
+        this.setState({
+            responses:this.state.responses < 5 ? this.state.responses + 1: 5
+        })
+    };
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score:0,
+            responses:0
+        })
+    }
+    componentDidMount(){
+        this.getQuestions();
+    }
+    
+    render () {
+        return(
+            <div className ="container">
+                <div className="title">اقراﺀ:The Islamic-Quiz</div>
+                {this.state.questionBank.length > 0 && 
+                    this.state.responses < 5 &&
+                    this.state.questionBank.map (
+                        ({question,answers,correct,questionId}) => (
+                            <QuestionBox 
+                            question={question}
+                            options={answers} 
+                            key={questionId}
+                            selected={answers => this.computeAnswer(answers,correct)}
+                            />
+                    )
+                    
+                )}
+                {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain }/>) : null}
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Isalamicquiz /> , document.getElementById("root"));
