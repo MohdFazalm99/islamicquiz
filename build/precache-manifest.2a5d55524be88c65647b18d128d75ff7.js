bb4ae16caf67261cbfb2f30506e44294274e3e24
Mohdfazalm99 <mohammadfazalm@gmail.com>
2020-05-30 03:59:17 +0530

    Adding new files

---
+++ build/precache-manifest.2a5d55524be88c65647b18d128d75ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db29e8d6db8276dec344a83ef69abca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42ea1f1860ade722d981",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "d9e90cc1ae1dbf8dbe7c",
+    "url": "/static/js/2.fe3ca96d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe3ca96d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ea1f1860ade722d981",
+    "url": "/static/js/main.2d3a31bb.chunk.js"
+  },
+  {
+    "revision": "6a37d5e554cab75be10b",
+    "url": "/static/js/runtime-main.f017ac5f.js"
+  }
+]);
